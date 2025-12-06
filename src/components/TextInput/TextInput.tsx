785c06e9105627fc77e46f96adb598e4d7ef687f
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-12-05 20:44:15 -0500

    docs: annotate TextInput.tsx with learning references

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -4,6 +4,7 @@
 // components/TextInput/TextInput.tsx
 // src/components/TextInput/TextInput.tsx
 // example taken from https://ps-lms.vercel.app/curriculum/se/415/lab-2#:~:text=Here%E2%80%99s%20a%20starting%20point%20for%20the%20TextInput%20component%20using%20Tailwind%20CSS%20classes%20for%20styling%3A
+// remember verbatimModuleSyntax false in tsconfigs.app.json and tsconfigs.node.json 
 import React from "react";
 import { TextInputProps } from "../../types";
 

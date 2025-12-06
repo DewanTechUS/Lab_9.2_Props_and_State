2e7dc7718b1395474068b09000036a586464c420
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-12-05 19:46:01 -0500

    feat: add CharacterCounterProps interface

---
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -0,0 +1,8 @@
+// https://ps-lms.vercel.app/curriculum/se/415/lab-2#:~:text=%7D-,3.%20CharacterCounter%20Component,-Create%20a%20CharacterCounter
+// types/index.ts
+// types/index.ts
+export interface CharacterCounterProps {
+  minWords?: number;
+  maxWords?: number;
+  targetReadingTime?: number; // in minutes
+}
